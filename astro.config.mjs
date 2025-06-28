4ece8a5efe3cf2063b39a4f734e5c9a10de8b439
QinengWang-Aiden <wongqineng73@zju.edu.cn>
2025-06-28 13:52:44 -0700

    Update config for mll-lab-nu deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ragen-ai.github.io",
-    base: '/',
+    site: "https://mll-lab-nu.github.io",
+    base: '/mind-cube/',
 });
